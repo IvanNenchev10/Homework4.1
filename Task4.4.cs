9c4344a3bd6d3aeedcc5a3e25c586d754d15a7e7
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-01 22:59:25 +0200

    Add files via upload

---
+++ Task4.4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Задача4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[50];
+            int k = 0;
+            for(int i=1;i<=100;i++)
+            {
+                if(i%2==0)
+                {
+                    arr[k] = i;
+                    k++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            for(int i=0;i<50;i++)
+            {
+                Console.WriteLine("Element {0} is:{1}", i + 1, arr[i]);
+            }
+        }
+    }
+}
